cf13ae5df6a40dee31e4adc26be6953022bbbd00
ed6095-web <eashandarsh77@gmail.com>
2026-02-23 11:23:53 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,32 +1,66 @@
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
+    public static boolean isPalindromeRecursive(String str) {
+        if (str.length() <= 1)
+            return true;
+
+        if (str.charAt(0) != str.charAt(str.length() - 1))
+            return false;
+
+        return isPalindromeRecursive(str.substring(1, str.length() - 1));
+    }
+
     public static void main(String[] args) {
 
-        String word = "civic";
+        Scanner scanner = new Scanner(System.in);
 
-        Deque<Character> deque = new LinkedList<>();
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
 
-        for (int i = 0; i < word.length(); i++) {
-            deque.add(word.charAt(i));
+        String processed = input.replaceAll("\\s+", "").toLowerCase();
+
+        System.out.println("\n----- Palindrome Results -----");
+
+        // UC1 - Basic Reverse Using StringBuilder
+        String reversed1 = new StringBuilder(processed).reverse().toString();
+        System.out.println("UC1 (StringBuilder Reverse): " + (processed.equals(reversed1)));
+
+        String reversed2 = "";
+        for (int i = processed.length() - 1; i >= 0; i--) {
+            reversed2 += processed.charAt(i);
         }
+        System.out.println("UC2 (Manual Loop): " + (processed.equals(reversed2)));
 
-        boolean isPalindrome = true;
+        boolean isPalindrome3 = true;
+        int left = 0;
+        int right = processed.length() - 1;
 
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
+        while (left < right) {
+            if (processed.charAt(left) != processed.charAt(right)) {
+                isPalindrome3 = false;
                 break;
             }
+            left++;
+            right--;
+        }
+        System.out.println("UC3 (Two Pointer): " + isPalindrome3);
+
+         Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < processed.length(); i++) {
+            stack.push(processed.charAt(i));
         }
 
-        if (isPalindrome) {
-            System.out.println("The given string \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The given string \"" + word + "\" is NOT a Palindrome.");
+        String reversed4 = "";
+        while (!stack.isEmpty()) {
+            reversed4 += stack.pop();
         }
+        System.out.println("UC4 (Stack Method): " + (processed.equals(reversed4)));
 
+        System.out.println("UC5 (Recursion): " + isPalindromeRecursive(processed));
+
+        scanner.close();
     }
 }
