52df7e257597454772dcd2fb08dc8f5b38c4d152
ed6095-web <eashandarsh77@gmail.com>
2026-02-23 09:35:13 +0530

    Updated UC1 - Welcome Message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,18 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("        PALINDROME CHECKER APP        ");
+        System.out.println("======================================");
+
+        System.out.println("Version : 1.0");
+        System.out.println("Developer : Eashan");
+
+        System.out.println("--------------------------------------");
+
+        System.out.println("Welcome to the Palindrome Checker Application.");
+        System.out.println("Application started successfully.");
+
+    }
 }
