059208f93d9bf5f85f683a9dcd8f909dcf9a2bf7
ed6095-web <eashandarsh77@gmail.com>
2026-02-23 09:20:12 +0530

    Updated PalindromeCheckerApp with latest changes

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Application Name
+        System.out.println("==================================");
+        System.out.println("      PALINDROME CHECKER APP      ");
+        System.out.println("==================================");
+
+        // Application Version
+        System.out.println("Version : 1.0");
+        System.out.println("Developer : Eashan");
+        System.out.println("----------------------------------");
+
+        // Application Description
+        System.out.println("This application checks whether a given");
+        System.out.println("string is a palindrome or not.");
+        System.out.println();
+
+        System.out.println("Application started successfully...");
+    }
 }
