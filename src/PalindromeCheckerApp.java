392017bfde825bb61931f08fd0e3f22ca3537394
christonowhere <christopherwilsonjust@gmail.com>
2026-03-02 08:52:24 +0530

    Implemented UC8 - Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -13,6 +13,72 @@ public class PalindromeCheckerApp {
         return isPalindromeRecursive(str.substring(1, str.length() - 1));
     }
 
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node createLinkedList(String str) {
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < str.length(); i++) {
+            Node newNode = new Node(str.charAt(i));
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        return head;
+    }
+
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public static boolean isPalindromeLinkedList(Node head) {
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -24,15 +90,14 @@ public class PalindromeCheckerApp {
 
         System.out.println("\n----- Palindrome Results -----");
 
-        // UC1 - Basic Reverse Using StringBuilder
         String reversed1 = new StringBuilder(processed).reverse().toString();
-        System.out.println("UC1 (StringBuilder Reverse): " + (processed.equals(reversed1)));
+        System.out.println("UC1 (StringBuilder Reverse): " + processed.equals(reversed1));
 
         String reversed2 = "";
         for (int i = processed.length() - 1; i >= 0; i--) {
             reversed2 += processed.charAt(i);
         }
-        System.out.println("UC2 (Manual Loop): " + (processed.equals(reversed2)));
+        System.out.println("UC2 (Manual Loop): " + processed.equals(reversed2));
 
         boolean isPalindrome3 = true;
         int left = 0;
@@ -48,7 +113,7 @@ public class PalindromeCheckerApp {
         }
         System.out.println("UC3 (Two Pointer): " + isPalindrome3);
 
-         Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         for (int i = 0; i < processed.length(); i++) {
             stack.push(processed.charAt(i));
         }
@@ -57,9 +122,12 @@ public class PalindromeCheckerApp {
         while (!stack.isEmpty()) {
             reversed4 += stack.pop();
         }
-        System.out.println("UC4 (Stack Method): " + (processed.equals(reversed4)));
+        System.out.println("UC4 (Stack Method): " + processed.equals(reversed4));
 
         System.out.println("UC5 (Recursion): " + isPalindromeRecursive(processed));
+
+        Node head = createLinkedList(processed);
+        System.out.println("UC8 (Linked List): " + isPalindromeLinkedList(head));
 
         scanner.close();
     }
